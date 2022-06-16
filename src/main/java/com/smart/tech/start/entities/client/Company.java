ae2012b7df28125c72850853fde40e826373eda6
Jakub Grajber <grajber.jakub@gmail.com>
2022-06-16 10:59:14 +0200

    Clean all code - prepare for starting from scratch

--- src/main/java/com/smart/tech/start/entities/client/Company.java
+++
@@ -1,8 +0,0 @@
-package com.smart.tech.start.entities.client;
-
-public class Company extends Client{
-
-    public Company(String firstname, String lastname){
-        super(firstname, lastname);
-    }
-}
--- src/main/java/com/smart/tech/start/entities/client/Person.java
+++
@@ -1,8 +0,0 @@
-package com.smart.tech.start.entities.client;
-
-public class Person extends Client{
-
-    public Person(String firstname, String lastname){
-        super(firstname, lastname);
-    }
-}
