32a81c727c1b7327b1605eea53bd1dce14d6409b
Jakub Grajber <grajber.jakub@gmail.com>
2022-06-18 16:49:18 +0200

    Update Spring Security config and make a controller for the login page

--- src/main/java/com/smart/tech/start/config/SecurityConfig.java
+++ src/main/java/com/smart/tech/start/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.smart.tech.start.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -11,8 +13,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
@@ -21,6 +26,26 @@ public class SecurityConfig {
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> {
+            try {
+                authz
+                        .anyRequest().authenticated()
+                        .and()
+                        .formLogin()
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        ).httpBasic(withDefaults());
+
+        return http.build();
     }
 
 }
---
+++ src/main/java/com/smart/tech/start/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.smart.tech.start.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("showMyLoginPage")
+    public String showLoginPage(){
+        return "login-page";
+    }
+}
