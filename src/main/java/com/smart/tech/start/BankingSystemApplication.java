5baf4e1da74c35396a189d287038bd6b649ff79b
Jakub Grajber <grajber.jakub@gmail.com>
2022-06-18 13:39:45 +0200

    Update pom file, add webapp directory, create HomeController

--- src/main/java/com/smart/tech/start/BankingSystemApplication.java
+++ src/main/java/com/smart/tech/start/BankingSystemApplication.java
@@ -3,12 +3,12 @@ package com.smart.tech.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BankingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankingSystemApplication.class, args);
     }
-
 }
