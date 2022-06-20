b39e37a0b320120da2654cf371321f3f3e3a4734
Jakub Grajber <grajber.jakub@gmail.com>
2022-06-20 22:35:31 +0200

    Update configs

--- src/main/java/com/smart/tech/start/BankingSystemApplication.java
+++ src/main/java/com/smart/tech/start/BankingSystemApplication.java
@@ -1,15 +1,17 @@
 package com.smart.tech.start;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaRepositories
+@SpringBootApplication
+//(exclude={DataSourceAutoConfiguration.class})
 public class BankingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankingSystemApplication.class, args);
+
     }
 }
--- src/main/java/com/smart/tech/start/config/SecurityConfig.java
+++ src/main/java/com/smart/tech/start/config/SecurityConfig.java
@@ -1,20 +1,17 @@
 package com.smart.tech.start.config;
 
-
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +25,11 @@ public class SecurityConfig {
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2/**");
     }
 
     @Bean
