e446a1e544d61d4072f384ae0230fc280a64e8bf
Jakub Grajber <grajber.jakub@gmail.com>
2022-06-24 22:20:42 +0200

    Use ConfigurationProperties to inject mail config values

---
+++ src/main/java/com/smart/tech/start/config/MailConfigProperties.java
@@ -0,0 +1,18 @@
+package com.smart.tech.start.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "mail")
+@Getter
+@Setter
+public class MailConfigProperties {
+
+    private String subject;
+    private String senderEmail;
+    private String emailTemplate;
+    private String encoding;
+}
--- src/main/java/com/smart/tech/start/email/EmailService.java
+++ src/main/java/com/smart/tech/start/email/EmailService.java
@@ -1,9 +1,8 @@
 package com.smart.tech.start.email;
 
+import com.smart.tech.start.config.MailConfigProperties;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -18,17 +17,18 @@ import javax.mail.internet.MimeMessage;
 public class EmailService implements EmailSender{
 
     private final JavaMailSender mailSender;
+    private final MailConfigProperties mailConfigProperties;
 
     @Override
     @Async
     public void send(String to, String email) {
         try{
             MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, mailConfigProperties.getEncoding());
             messageHelper.setText(email, true);
             messageHelper.setTo(to);
-            messageHelper.setSubject("Banking System Demo - User account confirmation");
-            messageHelper.setFrom("bsd@example.com");
+            messageHelper.setSubject(mailConfigProperties.getSubject());
+            messageHelper.setFrom(mailConfigProperties.getSenderEmail());
             mailSender.send(mimeMessage);
         } catch (MessagingException exc) {
             log.error("Failed to send email", exc);
