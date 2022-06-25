dcab767bb22507328cf6562164bdb7586e94327e
Jakub Grajber <grajber.jakub@gmail.com>
2022-06-25 13:34:58 +0200

    Add user registration controller test

---
+++ src/test/java/com/smart/tech/start/web/RegistrationControllerTest.java
@@ -0,0 +1,50 @@
+package com.smart.tech.start.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smart.tech.start.registration.registration.RegistrationRequest;
+import com.smart.tech.start.registration.user.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class RegistrationControllerTest {
+
+    private static final String FIRSTNAME = "James";
+    private static final String LASTNAME = "Hetfield";
+    private static final String PASSWORD = "password";
+    private static final String EMAIL = "j.h@example.com";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    JacksonTester<RegistrationRequest> registrationRequestTester;
+
+    @Test
+    @DisplayName("POST api/registration - register new user")
+    public void register_validUser_success() throws Exception {
+
+        RegistrationRequest request = new RegistrationRequest(FIRSTNAME, LASTNAME, PASSWORD, EMAIL);
+
+        mockMvc.perform(post("/api/registration")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(registrationRequestTester.write(request).getJson()))
+                .andExpect(status().isOk());
+    }
+
+}
