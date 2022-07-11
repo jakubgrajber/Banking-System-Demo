85e1e3f3ca0a96068b588736615566ff7d77f1c7
Jakub Grajber <grajber.jakub@gmail.com>
2022-07-11 17:56:17 +0200

    Configure eureka clients

--- notifications/src/main/java/com/smart/tech/start/NotificationsApplication.java
+++ notifications/src/main/java/com/smart/tech/start/NotificationsApplication.java
@@ -2,8 +2,10 @@ package com.smart.tech.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NotificationsApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationsApplication.class, args);
--- user/src/main/java/com/smart/tech/start/UserApplication.java
+++ user/src/main/java/com/smart/tech/start/UserApplication.java
@@ -2,13 +2,16 @@ package com.smart.tech.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.time.Clock;
 
-@EnableJpaRepositories
+
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableEurekaClient
 public class UserApplication {
 
     @Bean
