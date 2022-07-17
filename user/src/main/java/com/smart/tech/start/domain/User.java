f35fd9701443053de40017c848ac3e221cd310eb
Jakub Grajber <grajber.jakub@gmail.com>
2022-07-17 15:13:29 +0200

    Create User domain model with basic logic added

---
+++ user/src/main/java/com/smart/tech/start/domain/User.java
@@ -0,0 +1,100 @@
+package com.smart.tech.start.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User is a domain model that represents a client.
+ * It is used to manage the profile of the client and his accounts.
+ * There is no validation rules in this layer. All the validation is done in application layer.
+ * We can easily assume that we are not going to meet invalid data in User domain model.
+ */
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    private List<Account> accounts = new ArrayList<>(3);
+    private Boolean enabled = false;
+
+    public User(){
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void addAccount(Account account) {
+        if (!enabled)
+            throw new IllegalStateException("Cannot add an account for disabled User.");
+
+        accounts.add(account);
+    }
+
+    public void enable() {
+        if (enabled)
+            throw new IllegalStateException("The User has already been enabled.");
+        enabled = true;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void removeAccount(Account account) {
+        if (!enabled)
+            throw new IllegalStateException("Cannot remove account - User is disabled.");
+        if (!accounts.contains(account))
+            throw new IllegalStateException("There is no such account assigned to this User.");
+        if (account.getBalance() > 0.0)
+            throw new IllegalStateException("Cannot remove account with non-zero balance.");
+        accounts.remove(account);
+    }
+
+    public void disable() {
+        if (!enabled)
+            throw new IllegalStateException("The User has already been disabled.");
+        enabled = false;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
+    public void changeEmail(String newEmail) {
+        this.email = newEmail;
+    }
+}
---
+++ user/src/test/java/com/smart/tech/start/domain/UserTest.java
@@ -0,0 +1,233 @@
+package com.smart.tech.start.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest {
+
+    private static final double POSITIVE_NUMBER = 123.4;
+    private final static String FIRSTNAME = "James";
+    private final static String LASTNAME = "Hetfield";
+    private final static String PASSWORD = "password";
+    private final static String EMAIL = "j.h@example.com";
+
+    @Test
+    @DisplayName("Disabled user cannot register a new account")
+    public void shouldThrowIllegalStateException_WhenDisabledUserRegistersAccount() {
+        // GIVEN
+        User user = new User();
+        Account account = new Account();
+
+        // WHEN - THEN
+        Exception exception = assertThrows(IllegalStateException.class, () -> user.addAccount(account));
+        assertEquals("Cannot add an account for disabled User.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Enabled user can register a new account")
+    public void shouldBeAbleToRegisterAccount_WhenUserIsEnabled() {
+        // GIVEN
+        User user = new User();
+        Account account = new Account();
+        user.enable();
+
+        // WHEN
+        user.addAccount(account);
+
+        // THEN
+        assertEquals(1, user.getAccounts().size());
+    }
+
+    @Test
+    @DisplayName("New user should be disabled")
+    public void newUserShouldBeDisabled() {
+        //GIVEN
+        User user = new User();
+
+        assertFalse(user.isEnabled());
+    }
+
+    @Test
+    @DisplayName("New user can be enabled")
+    public void shouldBeAbleToEnableUser_WhenUserIsDisabled() {
+        // GIVEN
+        User user = new User();
+
+        // WHEN
+        user.enable();
+
+        // THEN
+        assertTrue(user.isEnabled());
+    }
+
+    @Test
+    @DisplayName("Enabled user cannot be enabled again")
+    public void shouldThrowIllegalStateException_WhenEnablesEnabledUser() {
+        // GIVEN
+        User user = new User();
+        user.enable();
+
+        // WHEN THEN
+        Exception exception = assertThrows(IllegalStateException.class, user::enable);
+        assertEquals("The User has already been enabled.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Enabled user can remove account when its balance equals zero")
+    public void shouldBeAbleToRemoveAccount_WhenAccountBalanceEqualsZero() {
+        // GIVEN
+        Account account = new Account();
+        User user = createEnabledUser();
+        user.addAccount(account);
+
+        // WHEN
+        user.removeAccount(account);
+
+        // THEN
+        assertEquals(0, user.getAccounts().size());
+    }
+
+    @Test
+    @DisplayName("Enabled user cannot to be enabled again")
+    public void shouldThrowIllegalStateException_WhenRemoveAccountWithBalanceThatNotEqualsZero() {
+        // GIVEN
+        Account account = new Account();
+        account.setBalance(POSITIVE_NUMBER);
+        User user = createEnabledUser();
+        user.addAccount(account);
+
+        // WHEN THEN
+        Exception exception = assertThrows(IllegalStateException.class, () -> user.removeAccount(account));
+        assertEquals("Cannot remove account with non-zero balance.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Disabled user cannot remove account")
+    public void shouldThrowIllegalStateException_WhenDisabledUserRemovesAccount() {
+        // GIVEN
+        Account account = new Account();
+        User user = createEnabledUser();
+        user.addAccount(account);
+        user.disable();
+
+        // WHEN THEN
+        Exception exception = assertThrows(IllegalStateException.class, () -> user.removeAccount(account));
+        assertEquals("Cannot remove account - User is disabled.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Enabled user cannot remove account which is not assign to him")
+    public void shouldThrowIllegalStateException_WhenRemoveAccountWithoutAddingSuchOneBefore() {
+        // GIVEN
+        Account account = new Account();
+        User user = createEnabledUser();
+
+        // WHEN THEN
+        Exception exception = assertThrows(IllegalStateException.class, () -> user.removeAccount(account));
+        assertEquals("There is no such account assigned to this User.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("New created user can be disabled")
+    public void shouldThrowIllegalStateException_WhenTryToDisableNewCreatedUser() {
+        // GIVEN
+        User user = new User();
+
+        // WHEN THEN
+        Exception exception = assertThrows(IllegalStateException.class, user::disable);
+        assertEquals("The User has already been disabled.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Enabled user can be disabled")
+    public void shouldBeAbleToDisableEnabledUser() {
+        // GIVEN
+        User user = createEnabledUser();
+
+        // WHEN
+        user.disable();
+
+        // THEN
+        assertFalse(user.isEnabled());
+    }
+
+    @Test
+    @DisplayName("User can be created with valid details")
+    public void shouldCreateUserProvidingValidDetails() {
+        // GIVEN
+        User user = new User(FIRSTNAME, LASTNAME, EMAIL, PASSWORD);
+
+        // THEN
+        assertEquals(FIRSTNAME, user.getFirstName());
+        assertEquals(LASTNAME, user.getLastName());
+        assertEquals(EMAIL, user.getEmail());
+        assertEquals(PASSWORD, user.getPassword());
+    }
+
+    @Test
+    @DisplayName("User can change his password providing new valid one")
+    public void shouldChangePassword_WhenNewOneIsValid() {
+        // GIVEN
+        User user = new User(FIRSTNAME, LASTNAME, EMAIL, PASSWORD);
+        String newPassword = "new-password";
+
+        // WHEN
+        user.changePassword(newPassword);
+
+        // THEN
+        assertEquals(newPassword, user.getPassword());
+    }
+
+    @Test
+    @DisplayName("User can change his email providing new valid one")
+    public void shouldChangeEmail_WhenNewOneIsValid() {
+        // GIVEN
+        User user = new User(FIRSTNAME, LASTNAME, EMAIL, PASSWORD);
+        String newEmail = "new@email.com";
+
+        // WHEN
+        user.changeEmail(newEmail);
+
+        // THEN
+        assertEquals(newEmail, user.getEmail());
+    }
+
+    @Test
+    @DisplayName("User can change his first name providing new valid one")
+    public void shouldChangeFirstName_WhenNewOneIsValid() {
+        // GIVEN
+        User user = new User(FIRSTNAME, LASTNAME, EMAIL, PASSWORD);
+        String newFirstName = "Kirk";
+
+        // WHEN
+        user.setFirstName(newFirstName);
+
+        // THEN
+        assertEquals(newFirstName, user.getFirstName());
+    }
+
+    @Test
+    @DisplayName("User can change his last name providing new valid one")
+    public void shouldChangeLastName_WhenNewOneIsValid() {
+        // GIVEN
+        User user = new User(FIRSTNAME, LASTNAME, EMAIL, PASSWORD);
+        String newLastName = "Hammett";
+
+        // WHEN
+        user.setLastName(newLastName);
+
+        // THEN
+        assertEquals(newLastName, user.getLastName());
+    }
+
+
+    private User createEnabledUser() {
+        User user = new User();
+        user.enable();
+        return user;
+    }
+
+}
