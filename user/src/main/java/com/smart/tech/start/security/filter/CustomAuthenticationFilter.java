139f9d16dc6302684b3a044399f348ef970ecfa3
Jakub Grajber <grajber.jakub@gmail.com>
2022-07-23 17:13:29 +0200

    Move response tokens from header to body

--- user/src/main/java/com/smart/tech/start/security/filter/CustomAuthenticationFilter.java
+++ user/src/main/java/com/smart/tech/start/security/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.smart.tech.start.security.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smart.tech.start.user.account.management.registration.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,7 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @AllArgsConstructor
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -51,8 +56,11 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 
-        response.setHeader("access_token", accessToken);
-        response.setHeader("refresh_token", refreshToken);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", accessToken);
+        tokens.put("refresh_token", refreshToken);
+        response.setContentType(APPLICATION_JSON_VALUE);
 
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
